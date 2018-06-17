9b103ce1cf713216646bb0c3e5eed00d618ce406
cam <git@camgould.com>
2018-06-18 09:51:56 +1000

    Gift card JS and acceesbility improvements for product image thumnails

--- src/assets/scripts/templates/gift_card.js
+++ src/assets/scripts/templates/gift_card.js
@@ -5,7 +5,7 @@
 * load when deployed to current issues with Slate v1
 **/
 
-alert('wtf')
+// alert('wtf')
 
 import "../../styles/templates/gift_card.scss";
 
@@ -16,11 +16,13 @@ import $ from 'jquery';
 * See https://github.com/soldair/node-qrcode
 **/
 import "qrcode/build/qrcode.js";
+var QRCode = require('qrcode')
 
 const config = {
-  // qrCode: '#QrCode',
   printButton: '#PrintGiftCard',
-  giftCardCode: '.giftcard__code',
+  giftCardCode: '#GiftCardCode',
+  QRdata: $('#QR').attr('data-identifier'),
+  QRwidth: 400,
 };
 
 $(document).ready(() => {
@@ -29,50 +31,32 @@ $(document).ready(() => {
 });
 
 // QR code for 2D barcode scanners
-var QRCode = require('qrcode')
-var QRdata = $('#QR').attr('data-identifier')
-var QRwidth = 400
-QRCode.toDataURL(QRdata, {
+QRCode.toDataURL(config.printButton, {
     color: {
       dark: '#000', // Black foreground
       light: '#0000' // Transparent background
     },
-    width: QRwidth
+    width: config.QRwidth
   }, function (err, url) {
-  $('#QR').html('<img src="' + url + '" style="width:' + QRwidth/2 + 'px">')
+  $('#QR').html('<img src="' + url + '" style="width:' + config.QRwidth/2 + 'px">')
 })
 
 // QRCode.toDataURL('Some text', function (err, url) {
 //   $('#QR').html('<img src="' + url + '">')
 // })
-
 // new QRCode($qrCode[0], {
 //   text: $qrCode.attr('data-identifier'),
 //   width: 120,
 //   height: 120,
 // });
 
-//
+// Print Gift Card action
 $(config.printButton).on('click', () => {
   window.print();
 });
 
-// Auto-select gift card code on click, based on ID passed to the function
-$(config.giftCardCode).on('click', {element: 'GiftCardDigits'}, selectText);
-
-function selectText(evt) {
-  const text = document.getElementById(evt.data.element);
-  let range = '';
-
-  if (document.body.createTextRange) {
-    range = document.body.createTextRange();
-    range.moveToElementText(text);
-    range.select();
-  } else if (window.getSelection) {
-    const selection = window.getSelection();
-    range = document.createRange();
-    range.selectNodeContents(text);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-}
+// Copy Gift Card code to clipboard automatically
+$(config.giftCardCode).click(function () {
+   $(this).select().val();
+   document.execCommand('copy');
+});
